efdcbf2d15204abdc366123f498ae9799247279c
KhomenkoDmitriy <khomenkodima100883@gmail.com>
2025-11-13 18:37:40 +0100

    SeleniumFirst_Homework12

---
+++ src/test/java/com/khomenko/testhomewrok/FirstTestHomework.java
@@ -0,0 +1,32 @@
+package com.khomenko.testhomewrok;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class FirstTestHomework {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        driver = new ChromeDriver();
+        driver.get("https://demowebshop.tricentis.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @Test
+    public void openSiteHomework(){
+       System.out.println("Site opened");
+    }
+
+    @AfterMethod(enabled = false)
+    public void tearDown(){
+        driver.quit();
+    }
+}
